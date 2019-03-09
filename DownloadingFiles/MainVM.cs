a9491574d4ec9790fe25b4c159a233b7bfea9b71
Ильдар Магжанов <ildar200103@rambler.ru>
2019-03-09 16:18:30 +0300

    Скачивание файлов

---
+++ DownloadingFiles/MainVM.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DownloadingFiles
+{ 
+    class MainVM : INotifyPropertyChanged
+    {
+        private string _url;
+        public string URL
+        {
+            get { return _url; }
+            set
+            {
+                _url = value;
+                OnPropertyChanged("Error");
+            }
+        }
+        private bool _openDownloadedFile;
+        public bool OpenDownloadedFile
+        {
+            get { return _openDownloadedFile; }
+            set
+            {
+                _openDownloadedFile = value;
+            }
+        }
+        private RelayCommand updateCommand;
+        public RelayCommand UpdateCommand
+        {
+            get
+            {
+                return updateCommand ??
+                    (updateCommand = new RelayCommand(Button_Click));
+            }
+        }
+        public void Button_Click(object obj)
+        {
+            if (_url != null)
+            {
+                Model.SelectFolder();
+                Model.DownloadFile(_url);
+            }
+        }
+        public string Error => Model.Error;
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected virtual void OnPropertyChanged(string prop="")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(prop));
+        }
+    }
+}
---
+++ DownloadingFiles/Model.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DownloadingFiles
+{
+    public static class Model
+    {
+        public static string Error;
+        public static string FilePath;
+        public static void SelectFolder()
+        {
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+            if (saveFileDialog.ShowDialog() == true)
+                FilePath = saveFileDialog.FileName;
+        }
+        public static void DownloadFile(string url)
+        {
+
+            try
+            {
+                WebClient myWebClient = new WebClient();
+                myWebClient.DownloadFile(url, FilePath);
+            }
+            catch (Exception e)
+            {
+                Error = "Ошибка при скачивании! Проверьте URL\n и не забудьте указать путь";
+            }
+        }
+    }
+}
